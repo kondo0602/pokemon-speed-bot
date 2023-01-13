db93b96bce983cd0625b904aa40b32aaaecfc657
kondo0602 <kondo8363@gmail.com>
2023-01-13 18:08:52 +0900

    chore: Next.jsのAPI Routesを使用してエンドポイントを書き換え

--- api/webhook/index.ts
+++ api/webhook/index.ts
@@ -1,7 +1,26 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { handleEvent } from "../../src/pokemon/handler";
+// import type { VercelRequest, VercelResponse } from "@vercel/node";
+// import { handleEvent } from "../../src/pokemon/handler";
 
-export default async function handler(req: VercelRequest, res: VercelResponse) {
+// export default async function handler(req: VercelRequest, res: VercelResponse) {
+//   // TODO: line.middlewareで行っているアクセストークンの検証を行う
+
+//   Promise.all(req.body.events.map(handleEvent))
+//     .then((result) => res.json(result))
+//     .catch((error) => {
+//       console.log(error);
+//     });
+// }
+
+// import { VercelRequest, VercelResponse } from "@vercel/node";
+import { NextApiRequest, NextApiResponse } from "next";
+import { handleEvent } from "../../src/pokemon";
+// import type { VercelRequest, VercelResponse } from "@vercel/node";
+// import { handleEvent } from "../../src/pokemon/handler";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   // TODO: line.middlewareで行っているアクセストークンの検証を行う
 
   Promise.all(req.body.events.map(handleEvent))
