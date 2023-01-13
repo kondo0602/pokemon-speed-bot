c615e05ab66b957057aac806ea03ab06630df985
kondo0602 <kondo8363@gmail.com>
2023-01-13 14:50:57 +0900

    fix: POSTリクエストに対して送信されたテキストを鸚鵡返しするように修正

--- api/webhook/index.ts
+++ api/webhook/index.ts
@@ -1,5 +1,8 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { handleEvent } from "../../src/pokemon/handler";
+import line from "@line/bot-sdk";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   Promise.all(req.body.events.map(handleEvent))
@@ -7,4 +10,22 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     .catch((error) => {
       console.log(error);
     });
+}
+
+export const config: line.ClientConfig = {
+  channelSecret: process.env.CHANNEL_SECRET as string,
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN as string,
+} as const;
+
+const client = new line.Client(config);
+
+async function handleEvent(event) {
+  if (event.type !== "message" || event.message.type !== "text") {
+    return Promise.resolve(null);
+  }
+
+  return client.replyMessage(event.replyToken, {
+    type: "text",
+    text: event.message.text, //実際に返信の言葉を入れる箇所
+  });
 }
