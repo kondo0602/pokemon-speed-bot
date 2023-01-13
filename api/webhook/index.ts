f9708ff2a61840e12d5d23c2abecd5bebeb793c2
kondo0602 <kondo8363@gmail.com>
2023-01-13 18:44:16 +0900

    feat: lineMiddlewareの適用

--- api/webhook/index.ts
+++ api/webhook/index.ts
@@ -14,14 +14,35 @@
 // import { VercelRequest, VercelResponse } from "@vercel/node";
 import { NextApiRequest, NextApiResponse } from "next";
 import { handleEvent } from "../../src/pokemon";
+import * as line from "@line/bot-sdk";
 // import type { VercelRequest, VercelResponse } from "@vercel/node";
 // import { handleEvent } from "../../src/pokemon/handler";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: any) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
+
+export const middlewareConfig: line.MiddlewareConfig = {
+  channelSecret: process.env.CHANNEL_SECRET as string,
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN as string,
+};
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  // TODO: line.middlewareで行っているアクセストークンの検証を行う
+  await runMiddleware(req, res, line.middleware(middlewareConfig));
 
   Promise.all(req.body.events.map(handleEvent))
     .then((result) => res.json(result))
