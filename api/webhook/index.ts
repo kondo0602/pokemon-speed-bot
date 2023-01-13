9c71caacbcadaaff2e18005a96c711696053cdd3
kondo0602 <kondo8363@gmail.com>
2023-01-13 15:44:10 +0900

    fix: src配下のhandleEventを呼ぶように修正

--- api/webhook/index.ts
+++ api/webhook/index.ts
@@ -1,6 +1,7 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import * as line from "@line/bot-sdk";
+// import * as line from "@line/bot-sdk";
 import * as dotenv from "dotenv";
+import { handleEvent } from "../../src/pokemon/handler";
 
 dotenv.config();
 
@@ -12,20 +13,20 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     });
 }
 
-export const config: line.ClientConfig = {
-  channelSecret: process.env.CHANNEL_SECRET as string,
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN as string,
-} as const;
+// export const config: line.ClientConfig = {
+//   channelSecret: process.env.CHANNEL_SECRET as string,
+//   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN as string,
+// } as const;
 
-const client = new line.Client(config);
+// const client = new line.Client(config);
 
-async function handleEvent(event: line.WebhookEvent) {
-  if (event.type !== "message" || event.message.type !== "text") {
-    return Promise.resolve(null);
-  }
+// async function handleEvent(event: line.WebhookEvent) {
+//   if (event.type !== "message" || event.message.type !== "text") {
+//     return Promise.resolve(null);
+//   }
 
-  return client.replyMessage(event.replyToken, {
-    type: "text",
-    text: event.message.text, //実際に返信の言葉を入れる箇所
-  });
-}
+//   return client.replyMessage(event.replyToken, {
+//     type: "text",
+//     text: event.message.text, //実際に返信の言葉を入れる箇所
+//   });
+// }
