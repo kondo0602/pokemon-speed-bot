d7a571411a36991eb336c8e483dfeaf4f68900b4
kondo0602 <kondo8363@gmail.com>
2023-01-13 10:43:32 +0900

    fix: return文を追加

--- dist/api/index.js
+++ dist/api/index.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = (request, response) => {
-    response.send("Hello LINE BOT (Serverless)");
+    return response.send("Hello LINE BOT (Serverless)");
 };
--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default (request: VercelRequest, response: VercelResponse) => {
-  response.send("Hello LINE BOT (Serverless)");
+  return response.send("Hello LINE BOT (Serverless)");
 };
