2e716fea59dee55d7f774c55b30f1dcfc263d923
kondo0602 <kondo8363@gmail.com>
2023-01-13 14:04:34 +0900

    remove: vercel.jsonの削除

--- dist/api/index.js
+++ dist/api/index.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = (request, response) => {
-    return response.send("Hello LINE BOT (Serverless)");
+    response.send("Hello LINE BOT (Serverless)");
 };
