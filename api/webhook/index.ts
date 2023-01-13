d35f15768d5a057c4250db031b8493f1335ccc30
kondo0602 <kondo8363@gmail.com>
2023-01-13 15:56:25 +0900

    remove: サーバーレス関数でリクエストを処理するため、express関連のモジュールを削除

--- api/webhook/index.ts
+++ api/webhook/index.ts
@@ -1,32 +1,12 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-// import * as line from "@line/bot-sdk";
-import * as dotenv from "dotenv";
 import { handleEvent } from "../../src/pokemon/handler";
 
-dotenv.config();
-
 export default async function handler(req: VercelRequest, res: VercelResponse) {
+  // TODO: line.middlewareで行っているアクセストークンの検証を行う
+
   Promise.all(req.body.events.map(handleEvent))
     .then((result) => res.json(result))
     .catch((error) => {
       console.log(error);
     });
 }
-
-// export const config: line.ClientConfig = {
-//   channelSecret: process.env.CHANNEL_SECRET as string,
-//   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN as string,
-// } as const;
-
-// const client = new line.Client(config);
-
-// async function handleEvent(event: line.WebhookEvent) {
-//   if (event.type !== "message" || event.message.type !== "text") {
-//     return Promise.resolve(null);
-//   }
-
-//   return client.replyMessage(event.replyToken, {
-//     type: "text",
-//     text: event.message.text, //実際に返信の言葉を入れる箇所
-//   });
-// }
--- src/app.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { router } from "@/routes";
-
-export const app = express();
-const port = process.env.PORT || 3000;
-
-app.use(router);
-
-app.listen(port);
