f65a22c19d4ca42ba9691a6c78d44e5c99bbf349
kondo0602 <kondo8363@gmail.com>
2023-01-14 23:43:28 +0900

    chore: vercelにビルド方法を伝えるためにbuildスクリプトを追加

--- api/webhook/index.ts
+++ api/webhook/index.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { handleEvent } from "@/pokemon";
 import * as line from "@line/bot-sdk";
 import { middlewareConfig } from "@/lib/line/config";
+import { handleEvent } from "@/pokemon";
 
 // @see: https://nextjs-ja-translation-docs.vercel.app/docs/api-routes/api-middlewares
 function runMiddleware(
