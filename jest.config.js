53610b3be79342442f3f9086575f21180e8df147
kondo0602 <kondo8363@gmail.com>
2023-01-14 23:25:06 +0900

    chore: src配下だけをテスト対象とするように修正

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
+  roots: ["<rootDir>/src"],
   preset: "ts-jest",
   testEnvironment: "node",
-  testPathIgnorePatterns: ["/dist/", "/node_modules/"],
 };
